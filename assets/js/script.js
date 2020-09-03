0234bef57b9c031d1569b4e1e45466067fcf23ff
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-09-03 14:35:49 -0600

    dynamically displays the city details.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var apiKey = "eb45e13bc37e0b171df80c35b694b693";
-var searchedStates = [];
+var searchedCitys = [];
 var cityCardEl = document.querySelector("#city-card");
 
 var getTodaysWeather = function(cityName) {
@@ -26,6 +26,7 @@ var displayTodaysWeather = function(weatherData, cityName) {
     clearElement(cityCardEl);
 
     displayTitle(weatherData, cityName);
+    displayDetails(weatherData);
 }
 
 var displayTitle = function(weatherData, cityName) {
@@ -35,7 +36,7 @@ var displayTitle = function(weatherData, cityName) {
 
     var cityTitleEL = document.createElement("h1");
     cityTitleEL.textContent = cityName + " " + today;
-    cityTitleEL.classList = "ml-3 mt-4";
+    cityTitleEL.classList = "ml-3 mt-4 col-12";
 
     var iconEl = document.createElement("img");
     iconEl.setAttribute("src", iconUrl);
@@ -43,6 +44,77 @@ var displayTitle = function(weatherData, cityName) {
     cityTitleEL.appendChild(iconEl);
 
     cityCardEl.appendChild(cityTitleEL);
+}
+
+var displayDetails = function(weatherData) {
+    
+    var detailsEl = document.createElement("div");
+    detailsEl.classList = "ml-3 col-12 mb-4";
+    detailsEl.setAttribute("id", "city-details")
+
+    var temperatureEl = document.createElement("p");
+    temperatureEl.textContent = "Temperature: " + weatherData.main.temp + " °F";
+    temperatureEl.classList = "city-detail";
+    detailsEl.appendChild(temperatureEl);
+
+    console.log(weatherData);
+
+    var humidityEl = document.createElement("p");
+    humidityEl.textContent = "Humidity: " + weatherData.main.humidity + "%";
+    humidityEl.classList = "city-detail";
+    detailsEl.appendChild(humidityEl);
+
+    var windSpeedEl = document.createElement("p");
+    windSpeedEl.textContent = "Wind Speed: " + weatherData.wind.speed + " MPH";
+    windSpeedEl.classList = "city-detail";
+    detailsEl.appendChild(windSpeedEl);
+
+    var lat = weatherData.coord.lat;
+    var lon = weatherData.coord.lon;
+    getTodaysUV(lat, lon, detailsEl);
+
+    cityCardEl.appendChild(detailsEl);
+}
+
+var displayUV = function(uv, detailsEl) {
+    var uvIndexEl = document.createElement("p");
+    uvIndexEl.textContent = "UV Index: ";
+    uvIndexEl.classList = "city-detail";
+
+    var uvBadgeEl = document.createElement("span");
+    uvBadgeEl.classList = "badge p-2";
+    uvBadgeEl.textContent = uv;
+
+    //determines what class to apply for danger rating of the UV index
+    if (uv <= 2) {
+        uvBadgeEl.classList += " badge-success";
+    } else if (uv <= 5 && uv > 2) {
+        uvBadgeEl.classList += " badge-warning";
+    } else if (uv <= 7 && uv > 5) {
+        uvBadgeEl.classList += " badge-caution";
+    } else if (uv <= 10 && uv > 7) {
+        uvBadgeEl.classList += " badge-danger";
+    } else if (uv > 10) {
+        uvBadgeEl.classList += " badge-doomed";
+    }
+    //
+
+    uvIndexEl.appendChild(uvBadgeEl);
+
+    detailsEl.appendChild(uvIndexEl);
+}
+
+var getTodaysUV = function(lat, lon, detailsEl) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+            displayUV(data.value, detailsEl);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    }); 
 }
 
 var clearElement = function(element) {

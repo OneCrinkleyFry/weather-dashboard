a766f01d6993a0d2bb1a1973f5b5483d263ab69f
JoshMiller5994 <talk2joshbgosh@gmail.com>
2020-09-02 23:07:21 -0600

    created a function to properly display the title element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 var apiKey = "eb45e13bc37e0b171df80c35b694b693";
 var searchedStates = [];
-var 
-
+var cityCardEl = document.querySelector("#city-card");
 
 var getTodaysWeather = function(cityName) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
@@ -9,7 +8,7 @@ var getTodaysWeather = function(cityName) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                displayTodaysWeather(data, cityName);
             });
         } else {
             alert("Error: " + response.statusText);
@@ -18,12 +17,39 @@ var getTodaysWeather = function(cityName) {
 }
 
 var getTodaysDate = function() {
-    return moment().format("MM/DD/YYYY");
+    var today = moment().format("(MM/DD/YYYY)");
+    return today;
 }
 
-var displayTodaysWeather = function(weather, cityName) {
+var displayTodaysWeather = function(weatherData, cityName) {
+    
+    clearElement(cityCardEl);
 
+    displayTitle(weatherData, cityName);
+}
+
+var displayTitle = function(weatherData, cityName) {
+    var today = getTodaysDate();
+    var icon = weatherData.weather[0].icon
+    var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+
+    var cityTitleEL = document.createElement("h1");
+    cityTitleEL.textContent = cityName + " " + today;
+    cityTitleEL.classList = "ml-3 mt-4";
+
+    var iconEl = document.createElement("img");
+    iconEl.setAttribute("src", iconUrl);
+
+    cityTitleEL.appendChild(iconEl);
+
+    cityCardEl.appendChild(cityTitleEL);
+}
+
+var clearElement = function(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
 }
 
 
-getTodaysWeather("atlanta");
+getTodaysWeather("Atlanta");
